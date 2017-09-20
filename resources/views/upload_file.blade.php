3b552a24827f959697e796de7729ab48cd101a20
MM <mahirhasancse@gmail.com>
2017-09-20 12:09:21 +0600

    File and Database Created

--- resources/views/upload_file.blade.php
+++ resources/views/upload_file.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app_dashboard')
 
 @section('content')
-<style type="text/css">
-.fileUpload {
-    position: relative;
-    overflow: hidden;
+<!-- <style type="text/css">
+    #customFile .custom-file-control:lang(en)::after {
+  content: "Select file...";
 }
-.fileUpload input.upload {
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 0;
-    padding: 0;
-    font-size: 20px;
-    cursor: pointer;
-    opacity: 0;
-    filter: alpha(opacity=0);
+
+#customFile .custom-file-control:lang(en)::before {
+  content: "Click me";
 }
-</style>
+
+/*when a value is selected, this class removes the content */
+.custom-file-control.selected:lang(en)::after {
+  content: "" !important;
+}
+
+.custom-file {
+  overflow: hidden;
+}
+.custom-file-control {
+  white-space: nowrap;
+}
+</style> -->
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -35,14 +39,19 @@
                 
                 <div style="margin-top: 20px;"></div>
                 
-                <label class="custom-file col-md-8">
-                  <input type="file" id="file" class="custom-file-input">
-                  <span class="custom-file-control"></span><i class="fa fa-folder-open" aria-hidden="true"></i>
-              </label>
+                
               <form>
-                <div class="form-group"> <!-- Message input !-->
-
-                </div>
+                <!-- <div class="form-group">
+                    <label class="custom-file" id="customFile">
+        <input type="file" class="custom-file-input" id="exampleInputFile" aria-describedby="fileHelp">
+        <span class="custom-file-control form-control-file"></span>
+    </label>
+                </div> -->
+                <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+  </div>
                 <div class="form-group"> <!-- Message input !-->
                     <label class="control-label " for="region_id">Message</label>
                     <select class="form-control" id="region_id" name="region">
@@ -57,19 +66,25 @@
                 
             </form>
             <a href="{{ url('/result')}}">
-                        <button class="btn btn-outline-success my-2 my-sm-0" >Submit</button>
-                    </a>
+                <button class="btn btn-outline-success my-2 my-sm-0" >Submit</button>
+            </a>
         </div>
     </div>
 </div>
 </div>
-<script type="text/javascript">
- .custom-file-control:lang(en)::after {
-  content: "Choose file...";
-}
+<!-- <script type="text/javascript">
 
-.custom-file-control:lang(en)::before {
-  content: "Browse";
-}
-</script>
+ show file value after file select 
+$('.custom-file-input').on('change',function(){
+  $(this).next('.form-control-file').addClass("selected").html($(this).val());
+})
+
+ method 2 - change file input to text input after selection
+$('.custom-file-input').on('change',function(){
+    var fileName = $(this).val();
+    $(this).next('.form-control-file').hide();
+    $(this).toggleClass('form-control custom-file-input').attr('type','text').val(fileName);
+})
+
+</script> -->
 @endsection
